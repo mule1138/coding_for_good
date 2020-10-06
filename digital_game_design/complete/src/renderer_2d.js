6ac96f9d83913c8f31ab9edd74b132002df225f8
Kevin Mueller <kmueller1138@gmail.com>
2020-10-06 17:24:54 +0200

    Added code to draw the maze

--- digital_game_design/complete/src/renderer_2d.js
+++ digital_game_design/complete/src/renderer_2d.js
@@ -14,19 +14,31 @@ export default class Renderer_2D {
         this.canvas = canvasElement;
         this.ctx = this.canvas.getContext('2d');
 
+        // Calculate the size of the character sprite
         const cellDims = maze.getCellDimensions();
         playerSprite.bbox.height = Math.floor(cellDims.height * 0.9);
         playerSprite.bbox.width = Math.floor(cellDims.width * 0.9);
     }
 
-    render() {
+    render(playerState) {
         const canvasCenter = {
             x: Math.floor(this.canvas.clientWidth / 2),
             y: Math.floor(this.canvas.clientHeight / 2)
         }
 
+        // Clear the canvas
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
         // Draw Background
         this.drawBackground(canvasCenter);
+
+        // Draw the maze
+        this.ctx.save();
+        this.drawMaze();
+
+        // Draw the player
+        this.drawPlayer(playerState);
+        this.ctx.restore();
     }
 
     drawBackground(canvasCenter) {
@@ -39,6 +51,34 @@ export default class Renderer_2D {
         bgRadialGradient.addColorStop(1.0, 'lightblue');
         this.ctx.fillStyle = bgRadialGradient;
         this.ctx.fillRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
+    }
+
+    drawMaze() {
+        // Draw a 2px border around the maze
+        const cellDims = this.maze.getCellDimensions();
+        const mazeWidthInUnits = cellDims.width * this.maze.width;
+        const mazeHeightInUnits = cellDims.height * this.maze.height;
+        this.ctx.fillStyle = 'black';
+        this.ctx.fillRect(0, 0, mazeWidthInUnits + 4, mazeHeightInUnits + 4);
+        this.ctx.fillStyle = 'gray';
+        this.ctx.fillRect(1, 1, mazeWidthInUnits + 2, mazeHeightInUnits + 2);
+        this.ctx.translate(2, 2);
+
+        let cellType = null;
+        let bbox = null;
+
+        for(let row = 0; row < this.maze.height; ++row) {
+            for(let col = 0; col < this.maze.width; ++col) {
+                cellType = this.maze.getCellType(row, col);
+                bbox = this.maze.getCellBoundingBox(row, col);
+                this.ctx.fillStyle = mazeCellTypes[cellType].bgColor;
+                this.ctx.fillRect(bbox.left, bbox.top, bbox.right - bbox.left, bbox.bottom - bbox.top);
+            }
+        }
+    }
+
+    drawPlayer(playerState) {
+        // this.ctx.fillthis.playerSprite
     }
 };
 
